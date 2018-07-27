9b8c4f14402e1c2243a5871b704bbd8f23643284
ttahgames <ttahgames@gmail.com>
2018-07-27 13:41:09 -0600

    Add files via upload

---
+++ all.js
@@ -0,0 +1,8 @@
+function w3_open() {
+    document.getElementById("mobileMenu").style.display = "block";
+    document.getElementById("menuButton").style.display = "none";
+}
+function w3_close() {
+    document.getElementById("mobileMenu").style.display = "none";
+    document.getElementById("menuButton").style.display = "block";
+}
